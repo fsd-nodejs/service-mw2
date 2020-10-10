3b7abf68007c41a51a511dc007c8ec17470b2fb2
tkvern <tkvern@qq.com>
2020-10-10 11:48:26 +0800

    增加中间件配置

---
+++ src/config/config.local.ts
@@ -0,0 +1,9 @@
+// redis配置
+export const redis = {
+  client: {
+    port: 6379, // Redis port
+    host: '192.168.12.130', // Redis host
+    password: '',
+    db: 0,
+  },
+};
--- src/config/plugin.ts
+++ src/config/plugin.ts
@@ -2,3 +2,14 @@ import { EggPlugin } from 'egg';
 export default {
   static: false, // default is true
 } as EggPlugin;
+
+// 启用redis
+export const redis = {
+  enable: true,
+  package: 'egg-redis',
+};
+
+export const jwt = {
+  enable: true,
+  package: '@waiting/egg-jwt',
+};
