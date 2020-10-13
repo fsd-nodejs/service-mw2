87702115c6afcfbb38f5ca913775011ee9fe7f69
tkvern <tkvern@qq.com>
2020-10-13 17:39:57 +0800

    error-handler 兼容 2.x

--- src/app/middleware/error-handler.ts
+++ src/app/middleware/error-handler.ts
@@ -10,8 +10,9 @@ export default () => {
     } catch (err) {
       // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
       ctx.app.emit('error', err, ctx);
+      let status = err.status || 500;
+      if (err.name === 'ValidationError') status = 422;
 
-      const status = err.status || 500;
       // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
       const error =
         status === 500 && ctx.app.config.env === 'prod'
@@ -21,7 +22,7 @@ export default () => {
       // 从 error 对象上读出各个属性，设置到响应中
       ctx.body = { code: status, mssage: error };
       if (status === 422) {
-        ctx.body.data = err.errors;
+        ctx.body.data = err.errors || err.details; // 兼容 midway 参数校验
       }
       ctx.status = status;
     }
