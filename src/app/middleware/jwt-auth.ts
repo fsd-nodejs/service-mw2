8d6a83e9525a77fe90d957fb17c4b2b0e24822ad
tkvern <tkvern@qq.com>
2020-10-14 12:07:21 +0800

    业务逻辑不再需要try catch

--- src/app/middleware/jwt-auth.ts
+++ src/app/middleware/jwt-auth.ts
@@ -7,29 +7,25 @@ import MyError from '@/app/util/my-error';
 // jwt auth
 export default () => {
   return async (ctx, next: IMidwayWebNext) => {
-    try {
-      const [, token] = ctx.header.authorization?.split(' ');
-      // 解密，获取payload
-      const { payload } = ctx.app.jwt.decode(token);
+    const [, token] = ctx.header.authorization?.split(' ');
+    // 解密，获取payload
+    const { payload } = ctx.app.jwt.decode(token);
 
-      const { jwtAuth } = ctx.app.config;
+    const { jwtAuth } = ctx.app.config.jwtAuth;
 
-      // redisToken不存在表示token已过期
-      const redisToken = await ctx.app.redis.get(
-        `${jwtAuth.redisScope}:accessToken:${payload.id}`
-      );
+    // redisToken不存在表示token已过期
+    const redisToken = await ctx.app.redis.get(
+      `${jwtAuth.redisScope}:accessToken:${payload.id}`
+    );
 
-      // 验证是否为最新的token
-      assert(token === redisToken, new MyError('Authentication Failed', 401));
+    // 验证是否为最新的token
+    assert(token === redisToken, new MyError('Authentication Failed', 401));
 
-      const userinfo = await ctx.app.redis.get(
-        `${jwtAuth.redisScope}:userinfo:${payload.id}`
-      );
+    const userinfo = await ctx.app.redis.get(
+      `${jwtAuth.redisScope}:userinfo:${payload.id}`
+    );
 
-      ctx.currentUser = JSON.parse(userinfo);
-    } catch (error) {
-      ctx.throw(error);
-    }
+    ctx.currentUser = JSON.parse(userinfo);
 
     await next();
   };
