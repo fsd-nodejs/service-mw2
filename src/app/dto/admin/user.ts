3aa5d409355c8cb9398b0824f64c160a271b30b8
tkvern <tkvern@qq.com>
2020-10-16 17:14:02 +0800

    新增user dto

--- src/app/dto/admin/user.ts
+++ src/app/dto/admin/user.ts
@@ -25,3 +25,56 @@ export class QueryDTO {
   )
   sorter: string;
 }
+
+export class ShowDTO {
+  @Rule(RuleType.string().trim().max(10).required())
+  id: string;
+}
+
+export class RemoveDTO {
+  @Rule(RuleType.array().items(RuleType.string().trim().max(10)).min(1))
+  ids: string[];
+}
+
+export class CreateDTO {
+  @Rule(RuleType.string().trim().min(5).max(190).required())
+  username: string;
+
+  @Rule(RuleType.string().trim().min(5).max(255).required())
+  name: string;
+
+  @Rule(RuleType.string().trim().max(255).optional())
+  avatar: string;
+
+  @Rule(RuleType.string().trim().min(5).max(60).required())
+  password: string;
+
+  @Rule(RuleType.array().items(RuleType.string().trim().max(10)).optional())
+  roles: string[];
+
+  @Rule(RuleType.array().items(RuleType.string().trim().max(10)).optional())
+  permissions: string[];
+}
+
+export class UpdateDTO {
+  @Rule(RuleType.string().trim().max(10).required())
+  id: string;
+
+  @Rule(RuleType.string().trim().min(5).max(190).required())
+  username: string;
+
+  @Rule(RuleType.string().trim().min(5).max(255).required())
+  name: string;
+
+  @Rule(RuleType.string().trim().max(255).optional())
+  avatar: string;
+
+  @Rule(RuleType.string().trim().min(5).max(60).optional())
+  password: string;
+
+  @Rule(RuleType.array().items(RuleType.string().trim().max(10)).optional())
+  roles: string[];
+
+  @Rule(RuleType.array().items(RuleType.string().trim().max(10)).optional())
+  permissions: string[];
+}
