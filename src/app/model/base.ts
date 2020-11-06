bcfa3778bcdc86229b482991578000e7530af774
tkvern <tkvern@qq.com>
2020-11-06 11:26:45 +0800

    增加注释

--- src/app/model/base.ts
+++ src/app/model/base.ts
@@ -1,5 +1,12 @@
 import { CreateDateColumn, UpdateDateColumn, AfterLoad } from 'typeorm';
 
+/**
+ * 基础的Model，对id字段默认会 转字符串处理
+ *
+ * 继承该Model的话，必须是有id字段的表
+ *
+ * 默认还会有crateAt、updatedAt
+ */
 export class BaseModel {
   id: string;
 
