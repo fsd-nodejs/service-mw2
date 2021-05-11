505fcbf51bc387665df462aef5aa16319d9ba8f5
waiting <1661926154@qq.com>
2021-05-11 20:09:38 +0800

    test: add common.test.ts

---
+++ test/util/common.test.ts
@@ -0,0 +1,16 @@
+import * as assert from 'assert'
+import { basename } from '@waiting/shared-core'
+
+import { retrieveExternalNetWorkInfo } from '../../src/app/util/common'
+
+
+const filename = basename(__filename)
+
+describe(filename, () => {
+  it('Should retrieveExternalNetWorkInfo works', async () => {
+    const infoList = retrieveExternalNetWorkInfo()
+    assert(infoList.length > 0)
+    console.log({ infoList })
+  })
+})
+
