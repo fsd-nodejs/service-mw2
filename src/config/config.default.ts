378a3c017ec4adca4ae0ab6a4d9af292dec81085
tkvern <tkvern@qq.com>
2021-07-29 13:38:34 +0800

    feat(Build): 增加 Custom Container 支持

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -31,7 +31,7 @@ export default (appInfo: EggAppInfo): DefaultConfig => {
   config.orm = {
     type: 'mysql',
     host: process.env.MYSQL_HOST || '127.0.0.1',
-    port: process.env.MYSQL_HOST || 3306,
+    port: process.env.MYSQL_PORT || 3306,
     username: process.env.MYSQL_USER || '',
     password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DATABASE || undefined,
