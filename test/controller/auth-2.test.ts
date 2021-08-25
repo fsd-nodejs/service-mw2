de28a25d850aa287a3c771aa047cfb67d7556cf9
waiting <1661926154@qq.com>
2021-08-25 18:44:59 +0800

    test: split file auth.test.ts

--- test/controller/auth-2.test.ts
+++ test/controller/auth-2.test.ts
@@ -1,9 +1,13 @@
+import { relative } from 'path';
+
 import { Framework } from '@midwayjs/web';
 import { createApp, close, createHttpRequest } from '@midwayjs/mock';
 
 import { Application } from '../../src/interface';
 
-describe('test/controller/auth.test.ts', () => {
+const filename = relative(process.cwd(), __filename).replace(/\\/ug, '/')
+
+describe(filename, () => {
   let app: Application;
   let currentUser;
 
@@ -47,45 +51,11 @@ describe('test/controller/auth.test.ts', () => {
     expect(response.body.code).toBe(422);
   });
 
-  it('should POST /auth/login by correct username and password', async () => {
-    const response = await createHttpRequest(app)
-      .post('/auth/login')
-      .type('form')
-      .send(app.config.admin)
-      .expect(200);
-    expect(response.body.data.token);
-    currentUser = response.body.data;
-  });
-
-  it('should GET /auth/currentUser', async () => {
-    const response = await createHttpRequest(app)
-      .get('/auth/currentUser')
-      .set('Authorization', `Bearer ${currentUser.token}`)
-      .expect(200);
-    expect(response.body.code).toBe(200);
-  });
-
   it('should GET 404', async () => {
     const response = await createHttpRequest(app)
       .get('/auth/currentUsersss')
       .set('Authorization', `Bearer ${currentUser.token}`)
       .expect(404);
     expect(response.body.code).toBe(404);
-  });
-
-  it('should GET /auth/logout', async () => {
-    const response = await createHttpRequest(app)
-      .get('/auth/logout')
-      .set('Authorization', `Bearer ${currentUser.token}`)
-      .expect(200);
-    expect(response.body.code).toBe(200);
-  });
-
-  it('should GET /auth/currentUser was logouted', async () => {
-    const response = await createHttpRequest(app)
-      .get('/auth/currentUser')
-      .set('Authorization', `Bearer ${currentUser.token}`)
-      .expect(401);
-    expect(response.body.code).toBe(401);
   });
 });
