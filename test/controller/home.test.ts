9363c0a5e971d49b466fc1f7003b9b0ece95048e
waiting <1661926154@qq.com>
2021-09-03 17:08:56 +0800

    test: add home.test.ts

---
+++ test/controller/home.test.ts
@@ -0,0 +1,95 @@
+import { relative } from 'path';
+import assert from 'assert';
+import { KoidComponent } from '@mw-components/koid';
+
+import { testConfig } from '../root.config'
+
+
+const filename = relative(process.cwd(), __filename).replace(/\\/ug, '/')
+
+describe(filename, () => {
+  let currentUser: any;
+
+  beforeAll(async () => {
+    const { app, httpRequest } = testConfig
+
+    assert(! currentUser.token)
+    const response = await httpRequest
+      .post('/auth/login')
+      .type('form')
+      .send(app.config.admin)
+      .expect(200);
+    currentUser = response.body.data;
+    assert(currentUser.token)
+  });
+
+
+  it('should GET /', async () => {
+    const { app, httpRequest } = testConfig
+
+    assert(currentUser.token)
+    const response = await httpRequest
+      .get('/')
+      .set('Authorization', `Bearer ${currentUser.token}`)
+      .expect(200);
+
+    const msg: string = response.text;
+    assert.ok(msg && msg.includes('Hello Midwayjs!'));
+    assert.ok(/reqId: "[1-9]\d{9,18}"/u.test(msg), msg); // 6755455236955799552
+  });
+
+  it('should GET /ping', async () => {
+    const { app, httpRequest } = testConfig
+
+    assert(currentUser.token)
+    const ret = await httpRequest.get('/ping').expect(200);
+
+    const msg: string = ret.text;
+    assert.ok(msg && msg === 'OK');
+  });
+
+  it('should GET /genid', async () => {
+    const { app, httpRequest } = testConfig
+
+    assert(currentUser.token)
+    const response = await httpRequest.get('/genid').expect(200);
+
+    const msg: string = response.text;
+    assert.ok(/[1-9]\d{9,18}/u.test(msg)); // 6755455236955799552
+
+    const ctx = app.createAnonymousContext();
+    const koid = await ctx.requestContext.getAsync(KoidComponent);
+
+    const info = koid.retrieveFromId(msg);
+    assert.ok(info.dataCenter === koid.config.dataCenter);
+    assert.ok(info.worker === koid.config.worker);
+  });
+
+  it('should GET /genidHex', async () => {
+    const { app, httpRequest } = testConfig
+
+    assert(currentUser.token)
+    const response = await httpRequest.get('/genidHex').expect(200);
+
+    const msg: string = response.text;
+    assert.ok(/[\dxa-f]{16}/u.test(msg), msg); // 5dc032befecd8000, 02a5f26eb5197000
+
+    const ctx = app.createAnonymousContext();
+    const koid = await ctx.requestContext.getAsync(KoidComponent);
+
+    const info = koid.retrieveFromId(msg);
+    assert.ok(info.dataCenter === koid.config.dataCenter);
+    assert.ok(info.worker === koid.config.worker);
+  });
+
+  it('should GET /sendToQueue', async () => {
+    const { app, httpRequest } = testConfig
+
+    assert(currentUser.token)
+    const response = await httpRequest
+      .get('/sendToQueue')
+      .set('Authorization', `Bearer ${currentUser.token}`)
+      .expect(200);
+    assert.ok(response.body.data);
+  });
+});
