1c1e8ecf2a2d96868cd047e21b890b31ff1579f1
luo jiyin <luojiyin@hotmail.com>
2021-09-10 09:53:01 +0800

    fix: rename HOST to PORT (#101)
    * fix HOST to PORT
    
    * fix HOST to PORT
    
    Co-authored-by: luojiyin <jiyin.luo@cikers.com>

--- src/config/config.unittest.ts
+++ src/config/config.unittest.ts
@@ -15,7 +15,7 @@ export const security = {
 export const orm: ConnectionOptions = {
   type: 'mysql',
   host: process.env.MYSQL_HOST || '127.0.0.1',
-  port: +process.env.MYSQL_HOST || 3306,
+  port: +process.env.MYSQL_PORT || 3306,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   database: process.env.MYSQL_DATABASE || 'shop_development',
